c0e067e90ff208bb94bb0feaab9679264651dae7
Lazaro <master_chief1237@hotmail.com>
2023-12-05 19:36:12 -0800

    cambios en archivos

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,8 +1,44 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+)
 
-func Hello(name string) string {
-	message := fmt.Sprintf("Hola, %v! Bienvenido!", name)
-	return message
+// Hello devuelve un saludo para la persona especificada.
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return name, errors.New("Nombre vacio")
+	}
+	//Devuelve un saludo que incluye el nombre en un mensaje
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+	return messages, nil
+}
+
+// randomFormat devuelve una de varios formatos de mensaje de saludo
+// se selecciona  de forma aleatoria
+func randomFormat() string {
+	formats := []string{
+		"Hola, %v! Bienvenido!\n",
+		"Que bueno verte, %v!\n",
+		"Saludos, %v, Encantado de conocerte!\n",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
